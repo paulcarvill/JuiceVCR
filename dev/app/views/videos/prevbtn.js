3a7fd3f4ac3e0e049886c29fd9774606b3892305
Paul Carvill <paul@paulcarvill.com>
2015-07-20 15:37:51 +0100

    removed rnadom and previous buttins. moved next button. added twitter meta tags

--- dev/app/views/videos/prevbtn.js
+++
@@ -1,9 +0,0 @@
-JuiceVCR.Views.PrevButtonView = Backbone.View.extend({
-	events: {
-      "click": "doclick"
-    },
-
-    doclick: function(){
-      window.app.skipVidBack();
-    }
-})
--- dev/app/views/videos/randbtn.js
+++
@@ -1,9 +0,0 @@
-JuiceVCR.Views.RandomButtonView = Backbone.View.extend({
-	events: {
-      "click": "doclick"
-    },
-
-    doclick: function(){
-      window.app.playRandom();
-    }
-})
